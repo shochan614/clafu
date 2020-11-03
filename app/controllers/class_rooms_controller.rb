ce792a5ca25ae11d888849c181a8ac540a608e3a
shochan614 <shochan.hina@gmail.com>
2020-11-03 12:47:47 +0900

    【学級登録機能】学級登録ページへの遷移

--- app/controllers/class_rooms_controller.rb
+++ app/controllers/class_rooms_controller.rb
@@ -1,2 +1,8 @@
 class ClassRoomsController < ApplicationController
+  def new
+    
+  end
+  def create
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: "menus#index"
   resources :menus, only: :index
   resources :schools, only: [:new, :create]
+  resources :class_rooms, only: [:new, :create]
 end
