daf015e76621277f6c5dc0ebb5bac5744a4ebe4b
shochan614 <shochan.hina@gmail.com>
2020-11-03 16:03:36 +0900

    【学級登録機能】schoolとclass_roomのアソシエーション

--- app/models/class_room.rb
+++ app/models/class_room.rb
@@ -3,4 +3,5 @@ class ClassRoom < ApplicationRecord
   validates :school, presence: true
   validates :school, presence: true
 
+  belongs_to :school
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,4 @@
 class School < ApplicationRecord
   validates :name, presence: true
+  has_many :class_rooms
 end
