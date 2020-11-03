f32c45cb80bd9865cc3aae8c9974af0bc9f15ad0
shochan614 <shochan.hina@gmail.com>
2020-11-03 11:30:24 +0900

    【学校登録機能】schoolモデル単体テストコード記述

--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe School, type: :model do
     before do
       @school = FactoryBot.build(:school)
     end
+    it '学校名が入力されていれば、登録できること' do
+      expect(@school).to be_valid
+    end
+    it '学校名が空白だと、登録できないこと' do
+      @school.name = nil
+      @school.valid?
+      expect(@school.errors.full_messages).to include("Name can't be blank")
+    end
   end
 end
