380377c8d4abf9687086cda82446fc761d6f40f4
shochan614 <shochan.hina@gmail.com>
2020-11-03 16:13:33 +0900

    【学級登録機能】バリデーションの正規表現

--- app/models/class_room.rb
+++ app/models/class_room.rb
@@ -1,8 +1,8 @@
 class ClassRoom < ApplicationRecord
   with_options presence: true do
     validates :school_id 
-    validates :grade
-    validates :class_number
+    validates :grade, format: { with: /\A[0-9]+\z/ }, inclusion: { in: 1..7 }
+    validates :class_number, format: { with: /\A[0-9]+\z/ }, inclusion: { in: 1..10 }
   end
 
   belongs_to :school
