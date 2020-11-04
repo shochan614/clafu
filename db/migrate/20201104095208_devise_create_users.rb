a743039d486a718519f556014ff68919b29e18e4
shochan614 <shochan.hina@gmail.com>
2020-11-04 19:00:43 +0900

    【ユーザー登録機能】usersテーブル生成

--- db/migrate/20201104095208_devise_create_users.rb
+++ db/migrate/20201104095208_devise_create_users.rb
@@ -4,6 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :first_name,         null: false
+      t.string :last_name,          null: false
+      t.string :nickname,           null: false
+      t.integer :attendance_number, null: false
+      t.references :class_room,     null: false, foreign_key: true
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_034811) do
+ActiveRecord::Schema.define(version: 2020_11_04_095208) do
 
   create_table "class_rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "school_id", null: false
@@ -28,5 +28,24 @@ ActiveRecord::Schema.define(version: 2020_11_03_034811) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "nickname", null: false
+    t.integer "attendance_number", null: false
+    t.bigint "class_room_id", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["class_room_id"], name: "index_users_on_class_room_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "class_rooms", "schools"
+  add_foreign_key "users", "class_rooms"
 end
