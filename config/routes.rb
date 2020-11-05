6ca59bad41102dfcc784e7477761478969b60130
shochan614 <shochan.hina@gmail.com>
2020-11-05 14:00:11 +0900

    【ユーザー登録機能】ニックネームでログインできる

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions:      'users/sessions'}
   root to: "menus#index"
   resources :menus, only: :index
   resources :schools, only: [:new, :create]
