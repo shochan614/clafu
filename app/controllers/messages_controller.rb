49f6b034f9ceb11feb58a7733e4ee92b1b0d8310
shochan614 <shochan.hina@gmail.com>
2020-11-11 15:49:39 +0900

    【メッセージの削除機能】削除できるように実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,5 @@
 class MessagesController < ApplicationController
   # before_action :reject  # 自分とは違うclass_roomのチャットルームには入らせない
-
   def index
     @message = Message.new
     @class_room = ClassRoom.find(params[:class_room_id])
@@ -16,6 +15,17 @@ class MessagesController < ApplicationController
       @messages = @class_room.messages.includes(:user)
       render :index
     end
+  end
+
+  def destroy
+    @message = Message.find(params[:id])
+    if @message.user.id == current_user.id
+      @message.destroy
+      redirect_to class_room_messages_path(current_user.class_room.id)
+    else
+      render :index
+    end
+
   end
 
 private
