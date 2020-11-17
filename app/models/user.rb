7f4a6ab92c687aacb82b8fca4716eb7e1d361742
shochan614 <shochan.hina@gmail.com>
2020-11-17 18:58:38 +0900

    【ユーザー登録機能】エラーメッセージの重複解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[A-z])(?=.*?[\d])[A-z\d]+\z/i.freeze # 半角英数混合1字以上
 
-  validates :password, length: { minimum: 6 }, format: { with: PASSWORD_REGEX }
+  validates :password, length: { minimum: 6, allow_blank: true }, format: { with: PASSWORD_REGEX, allow_blank: true }
   with_options presence: true do
     validates :nickname
     validates :first_name
