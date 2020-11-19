048778321b140ce173fcdc9ff9310dce2d1e6ecd
shochan614 <shochan.hina@gmail.com>
2020-11-19 14:01:09 +0900

    【学校登録機能】テストコード（エラー出）

--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -13,5 +13,11 @@ RSpec.describe School, type: :model do
       @school.valid?
       expect(@school.errors.full_messages).to include("学校名を入力してください")
     end
+    it '同じ学校名は登録できないこと' do
+      another_school = FactoryBot.build(:school)
+      another_school.name = @school.name
+      another_school.valid?
+      expect(another_school.errors.full_messages).to include("学校名はすでに存在します")
+    end
   end
 end
