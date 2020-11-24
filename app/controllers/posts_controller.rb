4aa7f0c7fb323369b7bb2d70dc7ccff997ec4dd1
shochan614 <shochan.hina@gmail.com>
2020-11-24 22:11:23 +0900

    【学級便り投稿機能】投稿できない場合のエラーを解消

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,17 +3,19 @@ class PostsController < ApplicationController
     @class_room = ClassRoom.find(params[:class_room_id])
     @posts = @class_room.posts.order("created_at DESC")
   end
+  
   def new
     @class_room = ClassRoom.find(params[:class_room_id])
     @post = Post.new
   end
+
   def create
     @class_room = ClassRoom.find(params[:class_room_id])
     @post = Post.new(post_params)
     if @post.save
       redirect_to class_room_posts_path(current_user.class_room.id)
     else
-      render :create
+      render :new
     end
   end
 
