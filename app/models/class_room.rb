bae0f5ced2434beba7b7e444830410ddcc652783
shochan614 <shochan.hina@gmail.com>
2020-11-24 18:52:28 +0900

    【学級便り投稿機能】アソシエーション記述

--- app/models/class_room.rb
+++ app/models/class_room.rb
@@ -2,6 +2,7 @@ class ClassRoom < ApplicationRecord
   belongs_to :school
   has_many :users
   has_many :messages
+  has_many :posts
 
   with_options presence: true do
     validates :grade, format: { with: /\A[0-9]+\z/, allow_blank: true}, inclusion: { in: 1..7, allow_blank: true }
