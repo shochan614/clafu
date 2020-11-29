0f27102c22ff9eedec167b621dfecd33b752e6f4
shochan614 <shochan.hina@gmail.com>
2020-11-29 18:21:19 +0900

    ゲストログインエラー解消

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-School.find_or_create_by!(id: 10, name: 'サンプル小学校')
+School.find_or_create_by!(id: 10, name: 'ゲスト小学校')
 ClassRoom.find_or_create_by!(id: 10, school_id: 10, grade: 6, class_number: 1, class_goals: '学級目標(サンプル)')
