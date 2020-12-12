0e9281b41cbb6da2ed4f85f96720e7cb158ed269
shochan614 <shochan.hina@gmail.com>
2020-12-12 21:57:08 +0900

    リファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user_id, only: [:show, :edit]
+
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
+  end
+
+  private
+
+  def set_user_id
+    @user = User.find(params[:id])
   end
 end
