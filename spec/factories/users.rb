1e0a84a49138839c044472dd1ca0f04061a541e0
shochan614 <shochan.hina@gmail.com>
2020-12-21 23:12:52 +0900

    【ユーザー登録機能】Facker記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :user do
-    
+    Faker::Config.locale = :ja
+    class_room_id { 1 }
+    email { Faker::Internet.free_email }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    nickname { Faker::Name.last_name }
+    attendance_number { Faker::Number.between(from: 1, to: 40) }
+    password = Faker::Internet.password(min_length: 6)
+    password { password }
+    password_confirmation { password }
+
   end
 end
