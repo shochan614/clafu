b6a3231370fa115e5f3d086dad5a94eccbb85bbd
shochan614 <shochan.hina@gmail.com>
2020-12-30 20:01:48 +0900

    【学校登録機能】全角ひらカタ漢字のみ登録可能にバリデーションを設定

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,6 @@
 class School < ApplicationRecord
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  ZENKAKU_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze # 全角ひらカタ漢字
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, format: { with: ZENKAKU_REGEX }
   has_many :class_rooms
 end
