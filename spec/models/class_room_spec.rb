18ddc93b03b922ce3f12450aeb627ac9885a35c5
shochan614 <shochan.hina@gmail.com>
2020-12-31 00:01:02 +0900

    【学級登録機能】モデル単体テスト準備

--- spec/models/class_room_spec.rb
+++ spec/models/class_room_spec.rb
@@ -3,11 +3,9 @@ require 'rails_helper'
 RSpec.describe ClassRoom, type: :model do
   describe '学級登録' do
     before do
-      @school = FactoryBot.create(:school)
       @class_room = FactoryBot.build(:class_room)
     end
     it '全ての項目が入力されていれば、登録できること' do
-      # expect(@class_room).to be_valid
     end
     it '必須項目が正しく入力されていれば、登録できること' do
     end
