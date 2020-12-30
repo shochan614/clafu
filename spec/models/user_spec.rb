77bf62ac8e34faa461a4877cb8e77ec145525323
shochan614 <shochan.hina@gmail.com>
2020-12-30 19:10:06 +0900

    【ユーザー登録機能】モデル単体テスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,39 +10,77 @@ RSpec.describe User, type: :model do
     end
 
     it 'クラスIDが空だと、登録することができないこと' do
-      
+      @user.class_room = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("クラスIDを入力してください")
     end
     it 'メールアドレスが空だと、登録することができないこと' do
-
+      @user.email = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("メールアドレスを入力してください")
     end
     it '上の名前が空だと、登録することができないこと' do
-
+      @user.first_name = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("上の名前を入力してください")
     end
     it '下の名前が空だと、登録することができないこと' do
-
+      @user.last_name = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("下の名前を入力してください")
     end
     it 'ニックネームが空だと、登録することができないこと' do
-
+      @user.nickname = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("ニックネームを入力してください")
     end
     it '出席番号が空だと、登録することができないこと' do
-
+      @user.attendance_number = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("出席番号を入力してください")
     end
     it 'パスワードが空だと、登録することができないこと' do
-
-    end
-    it 'パスワード（かくにん）が空だと、登録することができないこと' do
-
-    end
-
-    it '存在しないクラスIDでは登録することができないこと' do
-
+      @user.password = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("パスワードを入力してください")
     end
 
-    it '同じメールアドレスは登録することができないこと' do
-
+    it 'メールアドレスは、@を含む必要があること' do
+      @user.email.delete!('@')
+      @user.valid?
+      expect(@user.errors.full_messages).to include("メールアドレスは不正な値です")
     end
-    it '' do
 
+    it 'パスワードは、6文字以上での入力が必須であること' do
+      @user.password = '12345'
+      @user.password_confirmation = @user.password
+      @user.valid?
+      expect(@user.errors.full_messages).to include("パスワードは6文字以上で入力してください")
+    end
+    it 'パスワードは、半角英字がないとでの入力が必須であること' do
+      @user.password = '123456'
+      @user.password_confirmation = @user.password
+      @user.valid?
+      expect(@user.errors.full_messages).to include("パスワードは不正な値です")
+    end
+    it 'パスワードは、半角数字がないとでの入力が必須であること' do
+      @user.password = 'abcdef'
+      @user.password_confirmation = @user.password
+      @user.valid?
+      expect(@user.errors.full_messages).to include("パスワードは不正な値です")
+    end
+    it 'パスワードは全角数字では登録できないこと' do
+      @user.password = '１２３４５a'
+      @user.password_confirmation = @user.password
+      @user.valid?
+      expect(@user.errors.full_messages).to include("パスワードは不正な値です")
+    end
+
+    it 'パスワードとパスワード（かくにん）、値の一致が必須であること' do
+      @user.password = '12345a'
+      @user.password_confirmation = '12345b'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("パスワード（かくにん）とパスワードの入力が一致しません")
     end
   end
 end
