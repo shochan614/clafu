c7b45d1d967a65fc45dfa751381672a02af47196
shochan614 <shochan.hina@gmail.com>
2021-01-02 22:24:38 +0900

    【チャット機能】画像の投稿確認用FactoryBot記述（コメント）

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -5,5 +5,9 @@ FactoryBot.define do
 
     association :class_room
     association :user
+
+    # after(:build) do |content|
+    #   content.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    # end
   end
 end
