36d9fda6ece447e4fc29f5f237d7b73cd53a1549
shochan614 <shochan.hina@gmail.com>
2021-01-02 22:05:51 +0900

    【チャット機能】FactoryBot作成

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :message do
-    
+    Faker::Config.locale = :ja
+    content { Faker::Lorem.sentence }
+
+    association :class_room
+    association :user
   end
 end
