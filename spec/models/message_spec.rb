ba0680ee374ea6a82ea0fe2fad41a69fb257da62
shochan614 <shochan.hina@gmail.com>
2021-01-10 23:06:21 +0900

    【チャット機能】テスト準備

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  
+  describe 'メッセージ投稿' do
+    before do
+      @message = FactoryBot.build(:message)
+    end
+  it 'テキストがあれば、メッセージを送信することができる' do
+    
+  end
+  end
 end
