942985cc31eafaf13f12d01ceb2940f906531330
shochan614 <shochan.hina@gmail.com>
2021-01-11 15:32:58 +0900

    Ajaxを使えるように設定変更

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Clafu
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
