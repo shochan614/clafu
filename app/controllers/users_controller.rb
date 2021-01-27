331782f0962c1d7c802817e7287e7bba63570b51
shochan614 <shochan.hina@gmail.com>
2021-01-27 22:14:28 +0900

    【マイページ】不正なページ遷移を拒絶

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :reject  # 自分とは違うclass_roomのチャットルームには入らせない
   before_action :set_user_id, only: [:show, :edit]
 
   def show
@@ -14,5 +15,11 @@ class UsersController < ApplicationController
 
   def set_user_id
     @user = User.find(params[:id])
+  end
+  def reject
+    @class_room = ClassRoom.find(params[:class_room_id])
+    redirect_to root_path unless user_signed_in? && current_user.class_room.id == @class_room.id
+    # @user = User.find(params[:id])
+    # redirect_to root_path if current_user.id != @user.id
   end
 end
