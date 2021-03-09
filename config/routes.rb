b37a356695924c944102dab429c80e63e49f5491
shochan614 <shochan.hina@gmail.com>
2021-03-09 23:24:56 +0900

    ルーティングにコメント記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins #管理者
   devise_for :users, controllers: {
     sessions:      'users/sessions',
     registrations: 'users/registrations'
@@ -8,12 +8,12 @@ Rails.application.routes.draw do
     get 'users/guest_sign_in', to: 'users/sessions#new_guest' #ゲストユーザー用ログインアクション
   end
   root to: "class_rooms#index"
-  resources :schools, only: [:new, :create]
-  resources :class_rooms, only: [:index, :new, :create, :show] do
-    resources :messages, only: [:index, :create, :destroy]
-    resources :posts, only: [:index, :new, :create]
-    resources :users, only: [:show, :edit, :update]
-    resources :calendars, only: :show
+  resources :schools, only: [:new, :create] #学校登録
+  resources :class_rooms, only: [:index, :new, :create, :show] do #学級登録
+    resources :messages, only: [:index, :create, :destroy] #チャット
+    resources :posts, only: [:index, :new, :create] #学級通信
+    resources :users, only: [:show, :edit, :update] #ユーザー情報
+    resources :calendars, only: :show #カレンダー
   end
 
 end
